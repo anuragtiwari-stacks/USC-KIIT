f1424ef274ca8569cb513224c21daa05482dcc13
Anurag Tiwari <anuragtiwari12112004@gmail.com>
2026-06-12 11:14:07 +0530

    Add DAY_26 programs

---
+++ src/DAY_26/TowerOfHanoi.java
@@ -0,0 +1,31 @@
+package DAY_26;
+
+public class TowerOfHanoi
+{
+    public static void towerOfHanoi(int n, char source, char helper, char destination)
+    {
+        // Base Case
+        if (n == 1)
+        {
+            System.out.println("Move disk 1 from " + source + " to " + destination);
+            return;
+        }
+
+        // Move n-1 disks from source to helper
+        towerOfHanoi(n - 1, source, destination, helper);
+
+        // Move largest disk from source to destination
+        System.out.println("Move disk " + n + " from " + source + " to " + destination);
+
+        // Move n-1 disks from helper to destination
+        towerOfHanoi(n - 1, helper, source, destination);
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 3; // Number of disks
+
+        System.out.println("Steps to solve Tower of Hanoi:");
+        towerOfHanoi(n, 'A', 'B', 'C');
+    }
+}
