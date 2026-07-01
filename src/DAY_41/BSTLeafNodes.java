a84f85c85fc273a63f2762fcef0376d51b3ff6d5
Anurag Tiwari <anuragtiwari12112004@gmail.com>
2026-07-01 10:47:42 +0530

    Add DAY_41 BST practice solutions

---
+++ src/DAY_41/BSTLeafNodes.java
@@ -0,0 +1,37 @@
+package DAY_41;
+
+import java.util.*;
+
+public class BSTLeafNodes
+{
+    public static int countLeafNodes(Node root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null)
+        {
+            return 1;
+        }
+
+        return countLeafNodes(root.left) + countLeafNodes(root.right);
+    }
+
+    public static void main(String[] args)
+    {
+        Node root = new Node(50);
+
+        root.left = new Node(30);
+        root.right = new Node(70);
+
+        root.left.left = new Node(20);
+        root.left.right = new Node(40);
+
+        root.right.left = new Node(60);
+        root.right.right = new Node(80);
+
+        System.out.println("Leaf Nodes = " + countLeafNodes(root));
+    }
+}
